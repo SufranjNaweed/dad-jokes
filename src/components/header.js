fe979ac092988c0103f3fa4533529c1d9e1f87ac
SufranjNaweed <sufranj.naweed@gmail.com>
2020-09-20 01:32:54 +0200

    add : header & footer

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+const Header = () => {
+    return (
+        <header>
+            <h1>Dad Jokes</h1>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/favorites">Favorites</Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
