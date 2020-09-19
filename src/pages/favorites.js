3f6dcd7ecba03ee5899d26ef75a01643c03053f0
SufranjNaweed <sufranj.naweed@gmail.com>
2020-09-20 01:31:50 +0200

    add : home & favorites pages

---
+++ src/pages/favorites.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const favorites = () => {
+    return(
+        <section>
+            <h2>Favorites</h2>
+        </section>
+    )
+}
+
+export default favorites
---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+    return(
+        <section>
+            <h2>Home</h2>
+        </section>
+    )
+}
+
+export default home
