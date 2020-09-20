b338173bcc7a042e7a6195e48638ebeaded993bc
NaweedSufranj <sufranj.naweed@gmail.com>
2020-09-20 05:47:32 +0200

    fetch a joke & copy it into the clipboard

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
-const home = () => {
+const Home = () => {
+   const [joke, setJoke] = useState()
+
+    useEffect(() => {
+        fetchJoke()
+    }, [])
+
+    const fetchJoke = async () => {
+        try {
+            setJoke({})
+            const header = {
+                "Accept": "application/json"
+            }
+            const res =  await axios.get('https://icanhazdadjoke.com/', {headers: header})
+            if (res.data){
+                console.log(res.data)
+                setJoke(res.data)
+            }
+        }
+        finally {}
+    }
+
+    const copyClipboard = async () => {
+        try{
+            if(joke){
+                navigator.clipboard.writeText(joke.joke)
+            }
+        }
+        finally {}
+    }
+
     return(
         <section>
-            <h2>Home</h2>
+            <div className="container">
+                <h2>Home</h2>
+                <div className="jokes-container">
+                    <div className="joke-card">
+                        <div className="joke-title">
+                            <p>{joke ? joke.joke : "Loading Jokes"}</p>
+                        </div>
+                        <div className="joke-action-container">
+                            <button onClick={fetchJoke}>new joke</button>
+                            <button 
+                                onClick={copyClipboard}
+                            >share</button>
+                            <button>favorite</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
 
-export default home
+export default Home
