c4eecf501be6943dc8e3ed280c44a5462cae85ac
SufranjNaweed <sufranj.naweed@gmail.com>
2020-09-22 18:55:10 +0200

    update : fav page

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {JokesContext} from '../contexts/jokes'
+import JokeCard from '../components/JokeCard'
+import { ToastContainer } from 'react-toastify';
 
-const favorites = () => {
+const Favorites = () => {
+    const {jokes, deleteOneJokes } = useContext(JokesContext)
     return(
         <section>
-            <h2>Favorites</h2>
+            <div className="container">
+                <h2 className="page-title">Your favorites jokes</h2>
+                <div className="jokes-container">
+                    {
+                        jokes.map((joke) => (
+                            <JokeCard 
+                                key={joke.id}
+                                joke={joke} 
+                                deleteOneJokes={deleteOneJokes}
+                            />
+                        ))
+                    }
+                </div>
+                <ToastContainer position="bottom-center" />
+            </div>
         </section>
     )
 }
 
-export default favorites
+export default Favorites
