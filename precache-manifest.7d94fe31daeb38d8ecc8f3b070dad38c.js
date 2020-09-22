4d0c258552b483c890c059b338d055b0fd5bc8c7
SufranjNaweed <sufranj.naweed@gmail.com>
2020-09-22 19:16:43 +0200

    Updates

---
+++ precache-manifest.7d94fe31daeb38d8ecc8f3b070dad38c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef4e7029321e16626e67af49b95bd12",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "754358d82025da6e2e98",
+    "url": "/dad-jokes/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "f8941b3a483f74769206",
+    "url": "/dad-jokes/static/css/main.b2f787c3.chunk.css"
+  },
+  {
+    "revision": "754358d82025da6e2e98",
+    "url": "/dad-jokes/static/js/2.254ba969.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dad-jokes/static/js/2.254ba969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8941b3a483f74769206",
+    "url": "/dad-jokes/static/js/main.1e98fee1.chunk.js"
+  },
+  {
+    "revision": "0ee283f3a45f81336623",
+    "url": "/dad-jokes/static/js/runtime-main.751de3d0.js"
+  }
+]);
